adaf1d0932b4c295eaede02550d4cae21d753416
Utkarsh <utk2665@gmail.com>
2025-02-11 02:55:16 +0530

    Prisma setup

---
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { SignIn } from '@clerk/nextjs'
+
+export default function Page() {
+  return(
+    <div className='flex justify-center items-center h-screen'>
+        <SignIn />
+    </div>
+  )
+}
